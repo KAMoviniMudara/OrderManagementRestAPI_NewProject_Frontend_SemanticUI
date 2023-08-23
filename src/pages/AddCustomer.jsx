d1dc4457b51d59007cf449761eb21a7ddc0a5ad3
KAMoviniMudara <mudaramovini@gmail.com>
2023-08-23 11:03:07 +0530

    new

--- src/pages/AddCustomer.jsx
+++ src/pages/AddCustomer.jsx
@@ -1,80 +1,181 @@
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
-export const AddCustomer = (props)=>{
-   
-        const navigate = useNavigate();
-      
-        function handleClick (route){
-          navigate(route);
-        }
-        return(
-        <div >
-        
-        <form className="form">
-            
-            <lable className="login1" for="login">ADD CUSTOMER</lable>
-            <br/>
-            <table>
-                <tr>
-                    <td>
-                        <label> Name </label>
-                    </td>
-                    <td>
-                        <input className="input3" />
-                    </td>
-                    <td>
-                        <label>Password </label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
-                <tr className="dot">...............</tr>
-                <tr>
-                    <td>
-                        <label> Salary </label>
-                    </td>
-                    <td>
-                        <input className="input3" />
-                    </td>
-                    <td>
-                        <label>Address</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
-                <tr className="dot">...............</tr>
-                <tr>
-                    <td>
-                        <label> Contact Numbers </label>
-                    </td>
-                    <td>
-                        <input className="input3" />
-                    </td>
-                    <td>
-                        <label>Order_ID</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
+import { useState } from "react";
 
-                    <tr>           
-            </tr>
-            
-            </table>
+export const AddCustomer = (props) => {
+  const navigate = useNavigate();
 
-            <br></br>
-            <table>
-                <tr>
-                    <td></td>
-                    <td className="dot">........................................................................................................................</td>
-                    <td><button className = "bt3" >Save</button></td>
-                    <td><button className = "bt3" >Clear</button></td>
-                    <td><button className = "bt3" onClick={()=>handleClick("/Main")}>Back</button></td>
-                </tr>
-            </table>
-        </form>
-        </div>
-    )
-}
+  const [customer_name, setCustomerName] = useState("");
+  const [customer_address, setCustomerAddress] = useState("");
+  const [salary, setSalary] = useState("");
+  const [contact_numbers, setContactNumbers] = useState([]);
+  const [nic, setNic] = useState("");
+
+  const handleNicChange = (event) => {
+    const inputNic = event.target.value;
+    if (inputNic.length <= 10) {
+      setNic(inputNic);
+    } else {
+      alert("There are only 10 digits for NIC");
+    }
+  };
+  const handlePhoneNumberChange = (event, index) => {
+    const inputPhoneNumber = event.target.value;
+    if (/^[0-9]*$/.test(inputPhoneNumber)) {
+      if (inputPhoneNumber.length <= 10) {
+        const newContactNumbers = [...contact_numbers];
+        newContactNumbers[index] = inputPhoneNumber;
+        setContactNumbers(newContactNumbers);
+      } else {
+        alert("There are only 10 digits for phone number");
+      }
+    } else {
+      alert("Please enter numbers only for phone number");
+    }
+  };
+
+  const handleSalaryChange = (event) => {
+    const inputSalary = event.target.value;
+    if (/^[0-9]*$/.test(inputSalary)) {
+      setSalary(inputSalary);
+    } else {
+      alert("Please enter numbers only for salary");
+    }
+  };
+
+
+  const handleAddPhoneNumber = () => {
+    setContactNumbers([...contact_numbers, ""]);
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (contact_numbers.every(number => number.length === 10)) {
+      try {
+        await axios.post("http://localhost:8087/api/v1/customer/save", {
+          customerName: customer_name,
+          customerAddress: customer_address,
+          salary: salary,
+          contactNumbers: contact_numbers,
+          nic: nic,
+          activeState: true,
+        });
+        alert("Customer Registration Successful");
+        setCustomerName("");
+        setCustomerAddress("");
+        setSalary("");
+        setContactNumbers([]);
+        setNic("");
+      } catch (err) {
+        alert("Customer Registration Failed");
+      }
+    } else {
+      alert("Please enter exactly 10 digits for all phone numbers");
+    }
+  }
+
+  const handleClick = (route) => {
+    navigate(route);
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+      <lable className="login1" for="login">ADD CUSTOMER</lable>
+        <br />
+        <table>
+          <tr>
+            <td>
+              <label>Name </label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={customer_name}
+                onChange={(event) => setCustomerName(event.target.value)}
+              />
+            </td>
+            <td>
+              <label>Address</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={customer_address}
+                onChange={(event) => setCustomerAddress(event.target.value)}
+              />
+            </td>
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label> NIC </label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={nic}
+                onChange={handleNicChange}
+              />
+            </td>
+            <td>
+              <label>Salary</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={salary}
+                onChange={handleSalaryChange}
+              />
+            </td>
+           
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label> Contact Numbers </label>
+            </td>
+            <td colSpan="3">
+              {contact_numbers.map((number, index) => (
+                <input
+                  key={index}
+                  className="input3"
+                  value={number}
+                  onChange={(event) => handlePhoneNumberChange(event, index)}
+                />
+              ))}
+              <button type="button" onClick={handleAddPhoneNumber}>
+                Add Contact Number
+              </button>
+            </td>
+          </tr>
+          <tr>
+           
+          </tr>
+        </table>
+        <br />
+        <table>
+          <tr>
+            <td></td>
+            <td className="dot">
+              ........................................................................................................................
+            </td>
+            <td>
+              <button className="bt3" type="submit">
+                Save
+              </button>
+            </td>
+            <td>
+              <button className="bt3">Clear</button>
+            </td>
+            <td>
+              <button className="bt3" onClick={() => handleClick("/Main")}>
+                Back
+              </button>
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  );
+};
--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -1,90 +1,159 @@
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-export const AddItem = (props)=>{
-   
-        const navigate = useNavigate();
-      
-        function handleClick (route){
-          navigate(route);
-        }
-    return(
-        <div >
-        
-        <form className="form">
-            
-            <lable className="login1" for="login">ADD NEW ITEM</lable>
-            <br/>
-            <table>
-                <tr>
-                    <td>
-                        <label> Name </label>
-                    </td>
-                    <td>
-                        <input className="input3" />
-                    </td>
-                    <td>
-                        <label>Balance Quantity</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
-                <tr className="dot">...............</tr>
-                <tr>
-                    <td>
-                        <label> Measuring Unit </label>
-                    </td>
-                    <td>
-                    
-                        <select className="dropdown">
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3"  onClick={()=>handleClick("/Main")}>Option 3</option>
-                        </select>
-                    
-                    </td>
-                    <td>
-                        <label>Supplier price</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
-                <tr className="dot">...............</tr>
-                <tr>
-                    <td>
-                        <label className="dot">....................</label>
-                    </td>
-                    <td>
-                     
-                    </td>
-                    <td>
-                        <label>Seller Price</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
+const measuringUnitTypes = [
+  "KILO_GRAM",
+  "LITER",
+  "GRAM",
+  "MILI_GRAM",
+  "NUMBER"
+];
 
-                    <tr>           
-            </tr>
-            
-            </table>
+export const AddItem = (props) => {
+  const navigate = useNavigate();
 
-            <br></br>
-            <table>
-                <tr>
-                    <td></td>
-                    <td className="dot">........................................................................................................................</td>
-                    <td><button className = "bt3" >Save</button></td>
-                    <td><button className = "bt3" >Clear</button></td>
-                    <td><button className = "bt3" onClick={()=>handleClick("/Main")}>Back</button></td>
-                </tr>
-            </table>
-        </form>
-        </div>
-    )
+  const [item_name, setItemName] = useState("");
+  const [balance_qty, setBalanceQty] = useState("");
+  const [supplier_price, setSupplierPrice] = useState("");
+  const [seller_price, setSellerPrice] = useState("");
+  const [measure_type, setMeasureType] = useState("");
 
+  const validateNumberInput = (input) => {
+    return /^\d+$/.test(input);
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-}
+    if (
+      !validateNumberInput(balance_qty) ||
+      !validateNumberInput(supplier_price) ||
+      !validateNumberInput(seller_price)
+    ) {
+      alert("Please enter valid numeric values for quantity and prices.");
+      return;
+    }
+
+    try {
+      await axios.post("http://localhost:8087/api/v1/item/save", {
+        itemName: item_name,
+        measuringUnitType: measure_type,
+        balanceQty: balance_qty,
+        supplierPrice: supplier_price,
+        sellerPrice: seller_price,
+        activeState: true,
+      });
+      alert("Item Added Successful");
+      setItemName("");
+      setBalanceQty("");
+      setSupplierPrice("");
+      setSellerPrice("");
+      setMeasureType("");
+    } catch (err) {
+      alert("Item Added Failed");
+    }
+  };
+
+  const handleClick = (route) => {
+    navigate(route);
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+      <lable className="login1" for="login">ADD NEW ITEM</lable>
+        <br />
+        <table>
+          <tr>
+            <td>
+              <label> Name </label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={item_name}
+                onChange={(event) => setItemName(event.target.value)}
+              />
+            </td>
+            <td>
+              <label> Measuring Unit </label>
+            </td>
+            <td>
+              <select
+                className="dropdown"
+                value={measure_type}
+                onChange={(event) => setMeasureType(event.target.value)}
+              >
+                <option value="">Select Measuring Unit</option>
+                {measuringUnitTypes.map((unit) => (
+                  <option key={unit} value={unit}>
+                    {unit}
+                  </option>
+                ))}
+              </select>
+            </td>
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label>Balance Quantity</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={balance_qty}
+                onChange={(event) => setBalanceQty(event.target.value)}
+              />
+            </td>
+            <td>
+              <label>Supplier price</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={supplier_price}
+                onChange={(event) => setSupplierPrice(event.target.value)}
+              />
+            </td>
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label>Seller Price</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={seller_price}
+                onChange={(event) => setSellerPrice(event.target.value)}
+              />
+            </td>
+          </tr>
+        </table>
+        <br />
+        <table>
+          <tr>
+            <td></td>
+            <td className="dot">
+              ........................................................................................................................
+            </td>
+            <td>
+              <button className="bt3" type="submit">
+                Save
+              </button>
+            </td>
+            <td>
+              <button className="bt3">Clear</button>
+            </td>
+            <td>
+              <button className="bt3" onClick={() => handleClick("/Main")}>
+                Back
+              </button>
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  );
+};
