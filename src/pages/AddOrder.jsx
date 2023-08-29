6cc2d003e936195265139fb954a38efb509dd25d
KAMoviniMudara <mudaramovini@gmail.com>
2023-08-29 14:49:14 +0530

    Complete AddOrder

--- src/pages/AddOrder.jsx
+++ src/pages/AddOrder.jsx
@@ -1,86 +1,187 @@
-import { useNavigate } from "react-router-dom" 
-export const AddOrder = (props)=>{
-    const navigate = useNavigate();
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export const AddOrder = (props) => {
+  const navigate = useNavigate();
+  const [customers, setCustomers] = useState("");
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [itemName, setItemName] = useState("");
+  const [items,setItems] = useState("");
+  const [orders,setOrders] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [total, setTotal] = useState("");
+
+  const navigateToMain = () => {
+    navigate("/Main");
+  };
+
+
+    const handleSave = (event) => {
+        event.preventDefault();
       
-        function handleClick (route){
-          navigate(route);
-        }
-    
-    return(
-        <div >
-        
-        <form className="form">
-            
-            <lable className="login1" for="login">ADD NEW ORDER</lable>
-            <br/>
-            <table>
-                <tr>
-                    <td>
-                        <label>Date</label>
-                    </td>
-                    <td>
-                        <input className="input3" />
-                    </td>
-                   
-                    <td>
-                        <label> </label>
-                    </td>
-            
-                </tr>
-                <tr className="dot">...............</tr>
-                <tr>
-                    <td>
-                        <label>Amount</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                       
-                    <td>
-                        <label>Quantity</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
-                <tr className="dot">...............</tr>
-                <tr>
-                    <td>
-                    <label> Items </label>
-                    </td>
-                    <td>
-                        <select className="dropdown">
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3"  onClick={()=>handleClick("/Main")}>Option 3</option>
-                        </select>
-                    
-                    </td>
-                    
-                    
-                    <td>
-                        <label>Total</label>
-                    </td>
-                    <td>
-                        <input className="input3"/>
-                    </td>
-                </tr>
+        const orderData = {
+          customers: customers,
+          date: date,
+          orderDetails: [
+            {
+              amount: amount,
+              itemName: itemName,
+              items: items,
+              orders: orders,
+              qty: quantity,
+            },
+          ],
+          total: total,
+        };
+      
+        axios
+          .post("http://localhost:8087/api/v1/order/save", orderData)
+          .then((response) => {
+            alert("Order saved successfully");
+            handleClear(); // Clear the form fields after successful save
+          })
+          .catch((error) => {
+            alert("Failed to save order");
+          });
+  };
 
-                    <tr>           
-            </tr>
-            
-            </table>
+  const handleClear = () => {
+    setCustomers("");
+    setDate("");
+    setAmount("");
+    setQuantity("");
+    setItems("");
+    setItemName("");
+    setTotal("");
+    setOrders("");
+  };
 
-            <br></br>
-            <table>
-                <tr>
-                    <td></td>
-                    <td className="dot">........................................................................................................................</td>
-                    <td><button className = "bt3" >Save</button></td>
-                    <td><button className = "bt3" >Clear</button></td>
-                    <td><button className = "bt3"  onClick={()=>handleClick("/Main")} >Back</button></td>
-                </tr>
-            </table>
-        </form>
-        </div>
-    )}
+  return (
+    <div>
+      <form className="form">
+      <lable className="login1" for="login">ADD NEW ORDER</lable>
+        <br />
+        <table>
+          <tr>
+            <td>
+              <label>Date</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </td>
+            <td>
+              <label> Customers </label>
+            </td>
+            <input
+                className="input3"
+                value={customers}
+                onChange={(e) => setCustomers(e.target.value)}
+              />
+           
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label>Amount</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </td>
+            <td>
+              <label>Quantity</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+            </td>
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label>ItemName</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+              />
+            </td>
+            <td>
+              <label>Items</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={items}
+                onChange={(e) => setItems(e.target.value)}
+              />
+            </td>
+          </tr>
+          <tr className="dot">...............</tr>
+          <tr>
+            <td>
+              <label>Orders</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={orders}
+                onChange={(e) => setOrders(e.target.value)}
+              />
+            </td>
+            <td>
+              <label>Total</label>
+            </td>
+            <td>
+              <input
+                className="input3"
+                value={total}
+                onChange={(e) => setTotal(e.target.value)}
+              />
+            </td>
+          </tr>
+        </table>
+        <br />
+        <table>
+          <tr>
+            <td></td>
+            <td className="dot">
+              ..........................................................
+            </td>
+            <td>
+              <button className="bt3" onClick={handleSave}>
+                Save
+              </button>
+            </td>
+            <td>
+              <button className="bt3" onClick={handleClear}>
+                Clear
+              </button>
+            </td>
+            <td>
+              <button className="bt3" onClick={navigateToMain}>
+                Back
+              </button>
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
+  );
+};
+
+
