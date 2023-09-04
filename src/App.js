8a274704f1f1ea98a2e75bf4b1e7c6f1be00f0c2
KAMoviniMudara <mudaramovini@gmail.com>
2023-09-04 13:36:43 +0530

    Complete login with Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Main } from './pages/Main';
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,56 @@
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form, Grid, Header, Input, Segment } from "semantic-ui-react";
 
-export const Login = ()=>{
-const navigate = useNavigate();
+export const Login = () => {
+  const navigate = useNavigate();
 
-function handleClick(){
-    navigate("/Main")
-}
-    return(
-        <div className="login">
-        
-        <form className="form">
-            <lable className="login1" for="login">LOGIN</lable>
-            <br/>
-            <label htmlFor="username">User Name </label>
+  function handleClick() {
+    navigate("/Main");
+  }
 
-            <input className="input1" type="username" placeholder="abc@123" id="username" name="username"/>
-            <label htmlFor="password">Password </label>
-            <input className="input1" type="password" placeholder="**********" id="password" name="password"/>
-            <button className = "loginbt" onClick={(e)=>handleClick()}>Login</button>
-            
-        </form>
-        </div>
-    )
-}
+  return (
+    <div className="login">
+      <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: "450px" }}>
+          <Header as="h1" color="teal" textAlign="center">
+            LOGIN
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Field>
+                <Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                  id="username"
+                  name="username"
+                  size="massive" 
+                />
+              </Form.Field>
+              <Form.Field>
+                <Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  id="password"
+                  name="password"
+                  size="massive" 
+                />
+              </Form.Field>
+              <Button 
+                    color="teal" 
+                    fluid size="massive" 
+                    onClick={(e) => handleClick()}>
+                Login
+              </Button>
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
+};
