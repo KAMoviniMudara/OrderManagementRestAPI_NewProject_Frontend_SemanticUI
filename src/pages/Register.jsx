868c7f8603662dc8be48d9df833abc60223a9977
KAMoviniMudara <mudaramovini@gmail.com>
2023-10-19 16:09:20 +0530

    Login & Register done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { Button, Form, Grid, Header, Input, Segment } from "semantic-ui-react";
 
 export const Register = () => {
-  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     userName: "",
     email: "",
